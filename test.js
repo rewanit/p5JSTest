aa7522873298ab5e36660b32a694e68e62ec1622
rewanit <49819859+rewanit@users.noreply.github.com>
2019-08-21 01:03:27 +0300

    a

--- test.js
+++ test.js
@@ -1,14 +1,20 @@
-
+var slider1;
+var slider2;
+var slider3;
 function setup() {
   createCanvas(500, 500);
+  slider1 = createSlider(1,500,1);
+  slider2 = createSlider(1,500,1);
+  slider3 = createSlider(1,500,1);
+  
   background(51);
 }
 var ang =1;
 var kol = 2;
 var del=120;
 function draw() {
-  kol+=0.01;
-  del+=0.2;
+  kol=slider1.value();
+  del=slider2.value();
   var R = width/2;
   translate(width / 2, height / 2);
   rotate(radians(ang+=0.1));
@@ -16,7 +22,7 @@ function draw() {
   background(200);
   for(var l = 0;l<TWO_PI;l+=TWO_PI/del)
   {
-    line(Math.cos(l*0.2)*R,Math.sin(l*0.2)*R,Math.cos(l*kol)*R,Math.sin(l*kol)*R);
+    line(Math.cos(l)*R,Math.sin(l)*R,Math.cos(l*kol)*R,Math.sin(l*kol)*R);
   }
   
 }
