0ce9b07338161d7204aabce54fa01b2bf8028f58
rewanit <49819859+rewanit@users.noreply.github.com>
2019-08-21 02:21:34 +0300

    aaaa

--- test.js
+++ test.js
@@ -5,17 +5,18 @@ function setup() {
 var ang =1;
 var kol = 2;
 var del=120;
+var tmp=0;
 function draw() {
-
+tmp+=0.001
   var R = width/2;
   translate(width / 2, height / 2);
-  rotate(radians(ang+=0.1));
+  //rotate(radians(ang+=0.1));
   clear();  
   background(200);
-  var tmp=0;
   for(var l = 0;l<TWO_PI;l+=TWO_PI/del)
   {
-    line(Math.cos(l+mouseX/width*2)*R,Math.sin(l+mouseY/height*2)*R,Math.cos(l*kol+mouseX/width*2)*R,Math.sin(l*kol+mouseY/height*2)*R);
+    line(Math.cos(l+mouseX/width*2+tmp)*R,Math.sin(l+mouseY/height*2+tmp)*R,Math.cos(l*kol+mouseX/width*2+tmp)*R,Math.sin(l*kol+mouseY/height*2+tmp)*R);
+    line(Math.sin(l+mouseX/width*2-tmp)*R,Math.cos(l+mouseY/height*2-tmp)*R,Math.sin(l*kol+mouseX/width*2-tmp)*R,Math.cos(l*kol+mouseY/height*2-tmp)*R);
   }
   
 }
