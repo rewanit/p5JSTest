db43da8fb190c38ea4534fafed3b0d902809798f
rewanit <49819859+rewanit@users.noreply.github.com>
2019-08-22 15:59:57 +0300

    a!

--- test.js
+++ test.js
@@ -2,39 +2,45 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(25);
+
 }
 var ang = 0;
+var radius = 50;
 function draw() {
   clear();
   background(25);
-  ang+=1;
-  for (let i = 0; i < 360; i+=360/6) {
-    var rad = radians(i+ang+30);
-    drawHex(mouseX+Math.cos(rad)*(180*Math.sin(millis()/10000)),mouseY+Math.sin(rad)*(180*Math.sin(millis()/10000)),20,ang,Math.sin(millis()/1000)*128);
-    rad = radians(i+ang+15);
-    rad=-rad;
-    drawHex(mouseX+Math.cos(rad)*(260*Math.sin(millis()/1000)),mouseY+Math.sin(rad)*(260*Math.sin(millis()/1000)),20,ang,Math.sin(millis()/10000)*128);
-    
+  
+  var smesh = false;
+  for (let posY = 0; posY <= height+100; posY += radius * 2 * 3 / 4) {
+    smesh = !smesh;
+    for (let posX = 0; posX <= width+100; posX += sqrt(3) * radius  ) {
+      ang += 0.001;
+      drawHex(
+        posX+(smesh?-sqrt(3) * radius/2:0),
+        posY,
+        radius,
+        30+ang,
+        128+Math.sin(Math.random())*100);
+    }
   }
-  drawHex(mouseX,mouseY,20,ang,Math.sin(millis()/100)*200);
-  
-  
+  //drawHex(mouseX,mouseY,20,ang,Math.sin(-millis()/1000)*200);
+
+
 }
 
-function drawHex(posX,posY,radius,angle=0,color=255)
-{
+function drawHex(posX, posY, radius, angle = 0, color = 255) {
   fill(color);
   beginShape();
-  for (let i = 0; i < 360; i+=360/6) {
-    var rad = radians(i+angle);
-    vertex(posX+Math.cos(rad)*radius,posY+Math.sin(rad)*radius);    
+  for (let i = 0; i < 360; i += 360 / 6) {
+    var rad = radians(i + angle);
+    vertex(posX + Math.cos(rad) * radius, posY + Math.sin(rad) * radius);
   }
   return endShape(CLOSE);
 
 }
 
 function mouseClicked() {
-  
+
 }
 
 function windowResized() {
