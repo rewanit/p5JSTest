e7da042c7e568c936c9a5e8b4f70877663be528c
rewanit <49819859+rewanit@users.noreply.github.com>
2019-08-22 16:34:59 +0300

    HEX

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ function setup() {
   background(25);
 
 }
+var tmp=0;
 var ang = 0;
 var radius = 50;
 function draw() {
@@ -11,16 +12,22 @@ function draw() {
   background(25);
   
   var smesh = false;
+  var tmp_rad=radius;
+ 
   for (let posY = 0; posY <= height+100; posY += radius * 2 * 3 / 4) {
     smesh = !smesh;
+    tmp+=0.0001;
+    tmp_rad = millis()/10000;
+    var mils = millis();
     for (let posX = 0; posX <= width+100; posX += sqrt(3) * radius  ) {
       ang += 0.001;
+      
       drawHex(
         posX+(smesh?-sqrt(3) * radius/2:0),
         posY,
-        radius,
-        30+ang,
-        128+Math.sin(Math.random())*100);
+        radius/2-(Math.cos(posX+tmp_rad)*radius/2),
+        30,
+        128+(Math.cos(mils/1000)+0.2)*10);
     }
   }
   //drawHex(mouseX,mouseY,20,ang,Math.sin(-millis()/1000)*200);
@@ -30,6 +37,7 @@ function draw() {
 
 function drawHex(posX, posY, radius, angle = 0, color = 255) {
   fill(color);
+  noStroke();
   beginShape();
   for (let i = 0; i < 360; i += 360 / 6) {
     var rad = radians(i + angle);
