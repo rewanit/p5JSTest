dc9fba21583a2d6ed46de01c37123660fa3e9784
rewanit <49819859+rewanit@users.noreply.github.com>
2019-08-25 01:06:14 +0300

    ф

--- test.js
+++ test.js
@@ -6,20 +6,28 @@ var Hexagons = 40;
 var sensetive, maxRadius, minRadius;
 var bg;
 function setup() {
+  frameRate(60);
   biggest = (windowWidth <= windowHeight ? windowHeight : windowWidth);
   smaller = (windowWidth >= windowHeight ? windowHeight : windowWidth);
   radius = biggest / Hexagons;
   maxRadius = radius + 0.5;
   minRadius = radius * 0.5;
-  sensetive = 1;
+  sensetive = 2.3;
   bg = loadImage("bg.jpg");
   y = height / 2;
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('bg');
 }
-var x = 0;
-var y;
+var mode = false;
+function mousePressed() {
+  mode=!mode;
+}
 function draw() {
+  if (mode) {
+    mouseX=0;
+    mouseY=0;
+    
+  }
 
 
   background(bg);
@@ -40,28 +48,53 @@ function draw() {
   };
   var r;
   var mils = millis() / 3000;
+  //Hex Array Create
+  var HexArray = [];
   for (let posY = 0; posY <= height + 100; posY += radius * 2 * 3 / 4) {
     smesh = !smesh;
+    HexArray.push([]);
     for (let posX = 0; posX <= width + 100; posX += sqrt(3) * radius) {
       var Hex = {
         x: posX + (smesh ? -sqrt(3) * radius / 2 : 0),
         y: posY,
         color: (map(Math.sin(posY / 2000 + posX / 1000 - mils), -1, 1, -255, 255, true)),
         transparent: (map(Math.sin(posY / 2000 + posX / 1000 - mils), -1, 1, 170, 225, true)),
-        maxRad:maxRadius-(map(Math.sin(posY / 2000 + posX / 1000 - mils), -1, 1, -1, 1,true)),
-        minRad:minRadius
+        maxRadius: maxRadius - (map(Math.sin(posY / 2000 + posX / 1000 - mils), -1, 1, -1, 1, true)),
+        minRadius: minRadius,
+        radius: radius,
+        angle: 30
       }
-
-      drawHex(
-        Hex.x,
-        Hex.y,
-        //radius/2-Math.cos(posY/2000+posX/1000-mils )*radius/2,
-        map(dist(mouseX, mouseY, Hex.x, Hex.y) / (radius / Hexagons), radius, biggest / radius * (biggest / smaller) * sensetive, Hex.minRad, Hex.maxRad, true),
-        30,
-        Hex.color,
-        Hex.transparent);
+      HexArray[HexArray.length - 1].push(Hex);
     }
   }
+
+  
+
+  HexArray.forEach(innerArray => {
+    innerArray.forEach(
+      Hex => {
+        //Use transforms
+        Hex.radius = map(dist(mouseX, mouseY, Hex.x, Hex.y)/(radius*sensetive), 0, 1, Hex.minRadius, Hex.maxRadius, true);
+        if(mode)
+        {
+          Hex.maxRadius=Hex.maxRadius;
+          Hex.minRadius=0;
+          Hex.radius = map(abs(sin(dist(mouseX, mouseY, Hex.x, Hex.y)/(radius*sensetive)-mils)), 0, 1, Hex.minRadius, Hex.maxRadius, true);
+          
+        }
+        //DRAW!
+        drawHex(
+          Hex.x,
+          Hex.y,
+          Hex.radius,
+          Hex.angle,
+          Hex.color,
+          Hex.transparent);
+        //text(Hex.radius, Hex.x, Hex.y);
+      }
+    );
+
+  });
   //drawHex(mouseX,mouseY,20,ang,Math.sin(-millis()/1000)*200);
 
 
